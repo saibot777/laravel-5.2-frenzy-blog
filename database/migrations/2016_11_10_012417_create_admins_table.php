2ceada669af80659c3abb0939041a5df5e4549e9
Stefan <stefantrajkovic777@gmail.com>
2016-11-10 03:01:46 +0100

    database tables added, migrations and seeders...

--- database/migrations/2016_11_10_012417_create_admins_table.php
+++ database/migrations/2016_11_10_012417_create_admins_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
             $table->timestamps();
+            $table->rememberToken();
+            $table->string('email');
+            $table->string('password');
         });
     }
 
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('admins');
     }
 }
---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new \App\Admin();
+        $admin->email = "stefan@admin.com";
+        $admin->password = bcrypt("stefan");
+        $admin->save();
+    }
+}
